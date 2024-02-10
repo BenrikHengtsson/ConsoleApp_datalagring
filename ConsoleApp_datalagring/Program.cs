00cbd3b383390b1b709137a46d9a02ff87c26f77
Benke <henrik_junior@hotmail.com>
2024-02-10 14:42:44 +0100

    Add project files.

---
+++ ConsoleApp_datalagring/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*
+ Tried to upload my project to github from my macbook, but i use a wierd multi setup virtual maschine through Docker, so cant get it to work.
+ I will try again later, but im starting out with transfer the code bit for bit from the "g-uppgift example to see if it works."
+ */ 
